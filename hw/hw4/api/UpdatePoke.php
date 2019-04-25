02356769dc14281e1b9d84468695895a27ec067f
Abel <abgonzalez@csumb.edu>
2019-04-25 23:11:15 +0000

    updating lab7

--- hw/hw4/api/UpdatePoke.php
+++ hw/hw4/api/UpdatePoke.php
@@ -6,13 +6,13 @@ $conn = getDatabaseConnection("PokeSearch");
 $name = $_GET['name'];
 $Id = $_GET['id'];
 
-$sql = "INSERT INTO `Search` (`Name`, `ID`, `Look`) VALUES ('$name','$Id','1')";
+$sql = "INSERT INTO `Search` (`Name`, `pokeID`) VALUES ('$name','$Id')";
 $stmt = $conn->prepare($sql);
 $stmt->execute();
-// $sql = "UPDATE `Search` SET `Look` = 'Look' + 1 WHERE ID = $Id";
-// $stmt = $conn->prepare($sql);
-// $stmt->execute();
-//$records = $stmt->fetchAll(PDO::FETCH_ASSOC);
+$sql = "SELECT `Name` FROM `Search` ORDER BY Name";
+$stmt = $conn->prepare($sql);
+$stmt->execute();
+$records = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 echo json_encode($records);
 ?>

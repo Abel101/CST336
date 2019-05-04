6dedaf6db67ab758164dcbec799d5ff38ff30bd7
Abel <abgonzalez@csumb.edu>
2019-05-04 18:15:09 +0000

    updating lab 9

--- labs/lab9/index.php
+++ labs/lab9/index.php
@@ -1,33 +1,49 @@
 <?php
 if(!empty($_FILES)){
-    //print_r($_FILES);
-    echo"Image size:" . $_FILES['myFile']['size'];
-    move_uploaded_file($_FILES['myFile']['tmp_name'],"gallery/". $_FILES);
+   // print_r($_FILES);
+    
+   // echo"Image size:" . $_FILES['myFile']['size'];
+    
+    move_uploaded_file($_FILES['myFile']['tmp_name'],"gallery/". $_FILES['myFile']['name']);
 }
-function disaplyImagesUplaoded
-$imgaes = scandir("gallery");//Returns all file name within a folder
+    
+    function displayImagesUploaded(){
+    
+    $images = scandir("gallery");//Returns all file name within a folder
 
-for($i=2;$i<count($images);$i++){
-    echo "img src='gallery/$images[$i]' width='50'/>";
-    }
+    for($i=2;$i<count($images);$i++){
+        echo "<img src='gallery/$images[$i]' width='50'/>";
+        }//loop
+    }//function
+
 ?>
 <!DOCTYPE html>
+
+
 <html>
     <head>
         <title>Lab9 : File Upload </title>
+        <link rel="stylesheet" href="css/styles.css" type="text/css" />
     </head>
     <body>
-        
+        <h1> File Uploading </h1>
+        <br/>
+        <br/>
+        <br/>
         <form method ="POST" enctype="multipart/form-data">
           
-            
             <input type="file" name ="myFile"/>
             <button> Upload File </button>
              
         </form>
+        
         <hr>
+          <br/>
+        <br/>
+        <br/>
         <h3> Images Uploaded :</h3>
-        <0        
-
+        
+        <?= displayImagesUploaded() ?>       
+        
     </body>
 </html>
